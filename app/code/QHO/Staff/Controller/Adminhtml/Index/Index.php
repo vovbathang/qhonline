d3f01b3a1324d0ff912e27f03a9e23eb732b9f0d
thangnguyen <thangnb@smartosc.com>
2018-11-28 15:45:51 +0700

    commit edit sth

--- app/code/QHO/Staff/Controller/Adminhtml/Index/Index.php
+++ app/code/QHO/Staff/Controller/Adminhtml/Index/Index.php
@@ -1,36 +1,54 @@
 <?php
+
 namespace QHO\Staff\Controller\Adminhtml\Index;
+
 use Magento\Backend\App\Action\Context;
 use Magento\Framework\View\Result\PageFactory;
-class Index extends \Magento\Backend\App\Action{
-	protected $_resultPageFactory;
-	public function __construct(Context $context, PageFactory $pageFactory){
-		parent::__construct($context);
-		$this->_resultPageFactory=$pageFactory;
-	}
-	public function execute(){
-		$resultPage=$this->_resultPageFactory->create();
-		$resultPage->setActiveMenu("QHO_Staff::staff");
-		$resultPage->getConfig()->getTitle()->prepend(__("Staff Manager"));
 
+class Index extends \Magento\Backend\App\Action
+{
+    /**
+     * @var PageFactory
+     */
+    protected $_resultPageFactory;
 
-		/*$name=["Nguyen Van Teo","Nguyen Van Tun","Tran Van Tung","Truong Hoai Lam","Tran Quang Phong"];
-		$email=["vanteo@gmail.com","vantun@yahoo.com","tungtran@gmail.com","hoailam@gmail.com","quangphong@yahoo.com"];
-		$phone=["0903354595","0999351247","113","1174","1154"];
-		$position=["Director","Worker","Shipper","Support","Shipper"];
-		$avatar=["staff/abc.jpg","staff/bcd.jpg","staff/cde.jpg","staff/def.jpg","staff/efi.jpg"];
-		for($i=0;$i<5;$i++){
-			$staffModel=$this->_objectManager->create("QHO\Staff\Model\Staff");
-			$staffModel->addData([
-				"name" => $name[$i],
-				"email" => $email[$i],
-				"phone" => $phone[$i],
-				"position" => $position[$i],
-				"avatar" => $avatar[$i],
-				"status" => rand(0,1),
-			])->save();
-		}*/
+    /**
+     * Index constructor.
+     * @param Context $context
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        parent::__construct($context);
+        $this->_resultPageFactory = $pageFactory;
+    }
 
-		return $resultPage;
-	}
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface|\Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->_resultPageFactory->create();
+        $resultPage->setActiveMenu("QHO_Staff::staff");
+        $resultPage->getConfig()->getTitle()->prepend(__("Staff Manager"));
+
+        /*$name=["Nguyen Van Teo","Nguyen Van Tun","Tran Van Tung","Truong Hoai Lam","Tran Quang Phong"];
+        $email=["vanteo@gmail.com","vantun@yahoo.com","tungtran@gmail.com","hoailam@gmail.com","quangphong@yahoo.com"];
+        $phone=["0903354595","0999351247","113","1174","1154"];
+        $position=["Director","Worker","Shipper","Support","Shipper"];
+        $avatar=["staff/abc.jpg","staff/bcd.jpg","staff/cde.jpg","staff/def.jpg","staff/efi.jpg"];
+        for($i=0;$i<5;$i++){
+            $staffModel=$this->_objectManager->create("QHO\Staff\Model\Staff");
+            $staffModel->addData([
+                "name" => $name[$i],
+                "email" => $email[$i],
+                "phone" => $phone[$i],
+                "position" => $position[$i],
+                "avatar" => $avatar[$i],
+                "status" => rand(0,1),
+            ])->save();
+        }*/
+
+        return $resultPage;
+    }
 }
--- app/code/QHO/Staff/Controller/Adminhtml/Index/Save.php
+++ app/code/QHO/Staff/Controller/Adminhtml/Index/Save.php
@@ -1,76 +1,104 @@
 <?php
+
 namespace QHO\Staff\Controller\Adminhtml\Index;
+
 use Magento\Backend\App\Action\Context;
 use Magento\Framework\View\Result\PageFactory;
 use QHO\Staff\Model\StaffFactory;
 use Magento\Framework\Registry;
-class Save extends \Magento\Backend\App\Action{
-	protected $_resultPageFactory;
-	protected $_staffFactory;
-	protected $_coreRegistry;
-	public function __construct(
-					Context $context, 
-					PageFactory $pageFactory,
-					StaffFactory $staffFactory,
-					Registry $registry){
-		parent::__construct($context);
-		$this->_resultPageFactory=$pageFactory;
-		$this->_staffFactory=$staffFactory;
-		$this->_coreRegistry = $registry;
-	}
-	public function execute(){
-		$request=$this->getRequest();
-		$isDelete=0;
-		if($request->getPost()){
-			$staffModel=$this->_staffFactory->create();
-			$staffId=$request->getParam("id");
-			$formData=$request->getPostValue();
-			$urlRedirect="*/*/add";
-			if($staffId){
-				$staffModel->load($staffId);
-				$urlRedirect="*/*/edit/id/".$staffModel->getId();
-				if(isset($formData['avatar']['delete'])){
-					$isDelete=$formData['avatar']['delete'];	
-				}				
-				unset($formData['avatar']);
-			}
-			$staffModel->setData($formData);
 
-			$formFile=$request->getFiles()->toArray();
+class Save extends \Magento\Backend\App\Action
+{
+    /**
+     * @var PageFactory
+     */
+    protected $_resultPageFactory;
+    /**
+     * @var StaffFactory
+     */
+    protected $_staffFactory;
+    /**
+     * @var Registry
+     */
+    protected $_coreRegistry;
 
-			if($formFile['avatar']['name']){
+    /**
+     * Save constructor.
+     * @param Context $context
+     * @param PageFactory $pageFactory
+     * @param StaffFactory $staffFactory
+     * @param Registry $registry
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $pageFactory,
+        StaffFactory $staffFactory,
+        Registry $registry)
+    {
+        parent::__construct($context);
+        $this->_resultPageFactory = $pageFactory;
+        $this->_staffFactory = $staffFactory;
+        $this->_coreRegistry = $registry;
+    }
 
-				$imageHelper=$this->_objectManager->get("QHO\Staff\Helper\Image");
-				$imageFile=$imageHelper->uploadImage("avatar");
-				if($imageFile){
-					if($isDelete == 1){
-						$imageHelper->removeImage($staffModel->getAvatar());
-					}
-					$staffModel->setAvatar("staff/$imageFile");
-				}else{
-					$this->messageManager->addErrorMessage(__("Can not upload avatar, please try again"));
-					
-					if($staffId){
-						return $this->_redirect($urlRedirect);
-					}else{
-						$this->_getSession()->setFormData($formData);
-						return $this->_redirect($urlRedirect);
-					}
-				}
-			}else{
-				if(!$staffId){
-					$this->messageManager->addErrorMessage(__("You must upload staff avatar"));
-					$this->_getSession()->setFormData($formData);
-					return $this->_redirect($urlRedirect);					
-				}
-			}
-			$staffModel->save();
-            $this->_getSession()->setFormData(false);
-			$this->messageManager->addSuccessMessage(__("The staff information has been saved"));
-			if ($request->getParam("back")){
-                return $this->_redirect("*/*/edit",["id"=>$staffModel->getId(), "_current"=>true]);
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface
+     * @throws \Exception
+     */
+    public function execute()
+    {
+        $request = $this->getRequest();
+        $isDelete = 0;
+        if ($request->getPost()) {
+            $staffModel = $this->_staffFactory->create();
+            $staffId = $request->getParam("id");
+            $formData = $request->getPostValue();
+            $urlRedirect = "*/*/add";
+            if ($staffId) {
+                $staffModel->load($staffId);
+                $urlRedirect = "*/*/edit/id/" . $staffModel->getId();
+                if (isset($formData['avatar']['delete'])) {
+                    $isDelete = $formData['avatar']['delete'];
+                }
+                unset($formData['avatar']);
             }
-			return $this->_redirect("*/*/");
-		}
-	}
+            $staffModel->setData($formData);
+
+            $formFile = $request->getFiles()->toArray();
+
+            if ($formFile['avatar']['name']) {
+
+                $imageHelper = $this->_objectManager->get("QHO\Staff\Helper\Image");
+                $imageFile = $imageHelper->uploadImage("avatar");
+                if ($imageFile) {
+                    if ($isDelete == 1) {
+                        $imageHelper->removeImage($staffModel->getAvatar());
+                    }
+                    $staffModel->setAvatar("staff/$imageFile");
+                } else {
+                    $this->messageManager->addErrorMessage(__("Can not upload avatar, please try again"));
+
+                    if ($staffId) {
+                        return $this->_redirect($urlRedirect);
+                    } else {
+                        $this->_getSession()->setFormData($formData);
+                        return $this->_redirect($urlRedirect);
+                    }
+                }
+            } else {
+                if (!$staffId) {
+                    $this->messageManager->addErrorMessage(__("You must upload staff avatar"));
+                    $this->_getSession()->setFormData($formData);
+                    return $this->_redirect($urlRedirect);
+                }
+            }
+            $staffModel->save();
+            $this->_getSession()->setFormData(false);
+            $this->messageManager->addSuccessMessage(__("The staff information has been saved"));
+            if ($request->getParam("back")) {
+                return $this->_redirect("*/*/edit", ["id" => $staffModel->getId(), "_current" => true]);
+            }
+            return $this->_redirect("*/*/");
+        }
+    }
 }
