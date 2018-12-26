d081eaf3aac6b33a091c02f9d24c45b6c68c1ec3
vovbathang <vovbathang@gmail.com>
2018-12-27 02:32:54 +0700

    update test file

--- app/code/QHO/Hello/Controller/Index/Test.php
+++ app/code/QHO/Hello/Controller/Index/Test.php
@@ -5,3 +5,34 @@
  * Date: 13/11/2018
  * Time: 15:40
  */
+namespace QHO\Hello\Controller\Index;
+
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\Action\Action;
+
+class Test extends Action{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $_pageFactory;
+
+    /**
+     * Test constructor.
+     * @param Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $pageFactory
+     */
+    public function __construct(Context $context, \Magento\Framework\View\Result\PageFactory $pageFactory)
+    {
+        $this->_pageFactory= $pageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface|\Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        // TODO: Implement execute() method.
+       return $this->_pageFactory->create();
+    }
+}
