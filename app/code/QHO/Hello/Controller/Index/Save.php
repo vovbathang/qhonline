b7b8e6c0e364f0270d3349054d29519fa470b214
vovbathang <thangnb@smartosc.com>
2019-03-04 14:26:15 +0700

    add Save file

--- app/code/QHO/Hello/Controller/Index/Save.php
+++ app/code/QHO/Hello/Controller/Index/Save.php
@@ -8,8 +8,28 @@
 
 namespace QHO\Hello\Controller\Index;
 
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Registry;
+use Magento\Framework\View\Result\PageFactory;
+
 class Save extends Index
 {
+    /**
+     * Save constructor.
+     * @param Context $context
+     * @param RequestInterface $request
+     * @param PageFactory $pageFactory
+     * @param Registry $coreRegistry
+     */
+    public function __construct(Context $context, RequestInterface $request, PageFactory $pageFactory, Registry $coreRegistry)
+    {
+        parent::__construct($context, $request, $pageFactory, $coreRegistry);
+    }
+
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface|void
+     */
     public function execute()
     {
         $this->_forward('add');
